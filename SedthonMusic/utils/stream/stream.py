7f39963f8f2a2d5ea116675136cbbaf55dd740a9
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-27 23:36:34 +0300

    Update stream.py

--- SedthonMusic/utils/stream/stream.py
+++ SedthonMusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from SedthonMusic import Carbon, YouTube, app
+from SedthonMusic.core.call import Anony
+from SedthonMusic.misc import db
+from SedthonMusic.utils.database import add_active_video_chat, is_active_chat
+from SedthonMusic.utils.exceptions import AssistantErr
+from SedthonMusic.utils.inline import aq_markup, close_markup, stream_markup
+from SedthonMusic.utils.pastebin import AnonyBin
+from SedthonMusic.utils.stream.queue import put_queue, put_queue_index
+from SedthonMusic.utils.thumbnails import get_thumb
 
 
 async def stream(
