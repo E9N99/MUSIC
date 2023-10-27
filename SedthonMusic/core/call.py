2e88b00f956aa14ae8d628cfc042242acc23a75e
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-27 23:45:41 +0300

    Update call.py

--- SedthonMusic/core/call.py
+++ SedthonMusic/core/call.py
@@ -17,9 +17,9 @@ from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from AnonXMusic import LOGGER, YouTube, app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import (
+from SedthonMusic import LOGGER, YouTube, app
+from SedthonMusic.misc import db
+from SedthonMusic.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_lang,
@@ -31,11 +31,11 @@ from AnonXMusic.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
-from AnonXMusic.utils.inline.play import stream_markup
-from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
+from SedthonMusic.utils.exceptions import AssistantErr
+from SedthonMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
+from SedthonMusic.utils.inline.play import stream_markup
+from SedthonMusic.utils.stream.autoclear import auto_clean
+from SedthonMusic.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
