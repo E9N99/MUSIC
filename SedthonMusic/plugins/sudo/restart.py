ea4c8b02549387965fb6149a39094fad1866e20d
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-28 00:00:25 +0300

    Update restart.py

--- SedthonMusic/plugins/sudo/restart.py
+++ SedthonMusic/plugins/sudo/restart.py
@@ -10,15 +10,15 @@ from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import HAPP, SUDOERS, XCB
-from AnonXMusic.utils.database import (
+from SedthonMusic import app
+from SedthonMusic.misc import HAPP, SUDOERS, XCB
+from SedthonMusic.utils.database import (
     get_active_chats,
     remove_active_chat,
     remove_active_video_chat,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.pastebin import AnonyBin
+from SedthonMusic.utils.decorators.language import language
+from SedthonMusic.utils.pastebin import AnonyBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
