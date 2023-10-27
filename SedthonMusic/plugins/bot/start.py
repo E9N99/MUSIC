c0e3a4aed7cb0ee08b7abd927c7e97b2e611dc6b
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-27 23:51:05 +0300

    Update start.py

--- SedthonMusic/plugins/bot/start.py
+++ SedthonMusic/plugins/bot/start.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import _boot_
-from AnonXMusic.plugins.sudo.sudoers import sudoers_list
-from AnonXMusic.utils.database import (
+from SedthonMusic import app
+from SedthonMusic.misc import _boot_
+from SedthonMusic.plugins.sudo.sudoers import sudoers_list
+from SedthonMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +17,9 @@ from AnonXMusic.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from AnonXMusic.utils.decorators.language import LanguageStart
-from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
+from SedthonMusic.utils.decorators.language import LanguageStart
+from SedthonMusic.utils.formatters import get_readable_time
+from SedthonMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
