8d4ae5bae04f9e53844e7c431f1fe0e773a9dc80
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-27 23:50:39 +0300

    Update auth.py

--- SedthonMusic/plugins/admins/auth.py
+++ SedthonMusic/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import extract_user, int_to_alpha
-from AnonXMusic.utils.database import (
+from SedthonMusic import app
+from SedthonMusic.utils import extract_user, int_to_alpha
+from SedthonMusic.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AnonXMusic.utils.decorators import AdminActual, language
-from AnonXMusic.utils.inline import close_markup
+from SedthonMusic.utils.decorators import AdminActual, language
+from SedthonMusic.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
